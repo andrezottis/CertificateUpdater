841cc874dc46e654860c378b4406da9188a4dcf5
André Zottis <zottis.andre@outlook.com>
2019-05-07 20:30:14 -0300

    Changed some variables and methods names according to the pattern. Also, add CMD run.

--- CertificateUpdater/CertificateUpdater/Program.cs
+++ CertificateUpdater/CertificateUpdater/Program.cs
@@ -16,43 +16,49 @@ namespace CertificateUpdater
         {
             try
             {
-                string ISSUER = "SRVMETRIS";
-                string FINALTHUMBPRINT;
+                //Future must be read from a config file.
+                string Issuer = "SRVMETRIS";
+                string FinalThumbprint;
                 X509Store store = new X509Store("MY", StoreLocation.LocalMachine);
                 store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
 
                 X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
-                X509Certificate2Collection fcollection = (X509Certificate2Collection)collection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
-                X509Certificate2Collection apocollection = fcollection.Find(X509FindType.FindBySubjectName, ISSUER, true);
+                X509Certificate2Collection fCollection = (X509Certificate2Collection)collection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+                X509Certificate2Collection apoCollection = fCollection.Find(X509FindType.FindBySubjectName, Issuer, true);
 
-                X509Certificate2 lastCert = apocollection[0];
-                FINALTHUMBPRINT = lastCert.Thumbprint;
-                foreach (var certificatecolection in apocollection)
+                X509Certificate2 lastCert = apoCollection[0];
+                FinalThumbprint = lastCert.Thumbprint;
+                foreach (var certificatecolection in apoCollection)
                 {
 
                     var dateCert = Convert.ToDateTime(certificatecolection.GetExpirationDateString());
                     var compareDate = Convert.ToDateTime(lastCert.GetExpirationDateString());
-                    if ((apocollection.Count == 1) || (dateCert > compareDate))
+                    if ((apoCollection.Count == 1) || (dateCert > compareDate))
                     {
                         lastCert = certificatecolection;
                         Console.WriteLine("Certs is date : " + dateCert);
                         Console.WriteLine("The older date is:" + compareDate);
                         Console.WriteLine("The older cert thumb is:" + lastCert.Thumbprint);
-                        FINALTHUMBPRINT = lastCert.Thumbprint;
+                        FinalThumbprint = lastCert.Thumbprint;
                         //string strCmdText = "netsh http delete sslcert ipport=0.0.0.0:9000";
                         //Console.WriteLine(strCmdText);
                         //strCmdText = "netsh http add sslcert ipport=0.0.0.0:9000 certhash=" + FINALTHUMBPRINT.Trim() + " appid={3a487f59-fcb7-4213-b7ff-871456ae7b70}";
                         //Console.WriteLine(strCmdText);
                     }else
-                        FINALTHUMBPRINT = lastCert.Thumbprint;
+                        FinalThumbprint = lastCert.Thumbprint;
 
                     Console.WriteLine();
                 }
 
                 string strCmdText = "netsh http delete sslcert ipport=0.0.0.0:9000";
                 Console.WriteLine(strCmdText);
-                strCmdText = "netsh http add sslcert ipport=0.0.0.0:9000 certhash=" + FINALTHUMBPRINT.Trim() + " appid={3a487f59-fcb7-4213-b7ff-871456ae7b70}";
+                System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+                strCmdText = "netsh http add sslcert ipport=0.0.0.0:9000 certhash=" + FinalThumbprint.Trim() + " appid={3a487f59-fcb7-4213-b7ff-871456ae7b70}";
                 Console.WriteLine(strCmdText);
+                System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+
+
+                //WILL BE REMOVED AS SOON THE SOFTWARE IS WORK. HERE FOR CONSULTING PURPOSES.
 
                 //X509Certificate2Collection scollection = X509Certificate2UI.SelectFromCollection(apocollection, "Test Certificate Select", "Select a certificate from the following list to get information on that certificate", X509SelectionFlag.MultiSelection);
                 //Console.WriteLine("Number of certificates: {0}{1}", scollection.Count, Environment.NewLine);
