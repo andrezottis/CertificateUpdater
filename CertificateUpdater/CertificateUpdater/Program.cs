93ccfb0856b1359a646471483344eb5f25bb5ce3
André Zottis <zottis.andre@outlook.com>
2019-05-08 08:08:03 -0300

    Start to remove the hardcode configuration.

--- CertificateUpdater/CertificateUpdater/Program.cs
+++ CertificateUpdater/CertificateUpdater/Program.cs
@@ -9,7 +9,15 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace CertificateUpdater
+
 {
+
+    class Constants
+    {
+        public const string Issuer = "METRISHOTFIX";
+        public const int ServicePort = 9000;
+        public const string ServiceAppGUID ="3a487f59-fcb7-4213-b7ff-871456ae7b70";
+    }
     class Program
     {
         static void Main(string[] args)
@@ -17,65 +25,74 @@ namespace CertificateUpdater
             try
             {
                 //Future must be read from a config file.
-                string Issuer = "METRISHOTFIX";
                 string FinalThumbprint;
+
                 X509Store store = new X509Store("MY", StoreLocation.LocalMachine);
                 store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
 
                 X509Certificate2Collection collection = (X509Certificate2Collection)store.Certificates;
                 X509Certificate2Collection fCollection = (X509Certificate2Collection)collection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
-                X509Certificate2Collection apoCollection = fCollection.Find(X509FindType.FindBySubjectName, Issuer, true);
+                X509Certificate2Collection apoCollection = fCollection.Find(X509FindType.FindBySubjectName, Constants.Issuer, true);
 
-                X509Certificate2 lastCert = apoCollection[0];
-                FinalThumbprint = lastCert.Thumbprint;
-                foreach (var certificatecolection in apoCollection)
+
+                if (0 == apoCollection.Count)
+                {
+                    Console.WriteLine("Error: No certificate found containing the specified requirements. ");
+                    Console.ReadLine();
+                }
+                else
                 {
 
-                    var dateCert = Convert.ToDateTime(certificatecolection.GetExpirationDateString());
-                    var compareDate = Convert.ToDateTime(lastCert.GetExpirationDateString());
-                    if ((apoCollection.Count == 1) || (dateCert > compareDate))
+                    X509Certificate2 lastCert = apoCollection[0];
+                    FinalThumbprint = lastCert.Thumbprint;
+
+                    foreach (var certificatecolection in apoCollection)
                     {
-                        lastCert = certificatecolection;
-                        Console.WriteLine("Certs is date : " + dateCert);
-                        Console.WriteLine("The newest date is:" + compareDate);
-                        Console.WriteLine("The newest cert thumb is:" + lastCert.Thumbprint);
-                        FinalThumbprint = lastCert.Thumbprint;
-                        //string strCmdText = "netsh http delete sslcert ipport=0.0.0.0:9000";
-                        //Console.WriteLine(strCmdText);
-                        //strCmdText = "netsh http add sslcert ipport=0.0.0.0:9000 certhash=" + FINALTHUMBPRINT.Trim() + " appid={3a487f59-fcb7-4213-b7ff-871456ae7b70}";
-                        //Console.WriteLine(strCmdText);
-                    }else
-                        FinalThumbprint = lastCert.Thumbprint;
 
-                    Console.WriteLine();
+                        var dateCert = Convert.ToDateTime(certificatecolection.GetExpirationDateString());
+                        var compareDate = Convert.ToDateTime(lastCert.GetExpirationDateString());
+                        if ((apoCollection.Count == 1) || (dateCert > compareDate))
+                        {
+                            lastCert = certificatecolection;
+                            Console.WriteLine("Certs is date : " + dateCert);
+                            Console.WriteLine("The newest date is:" + compareDate);
+                            Console.WriteLine("The newest cert thumb is:" + lastCert.Thumbprint);
+                            FinalThumbprint = lastCert.Thumbprint;
+
+                        }
+                        else
+                            FinalThumbprint = lastCert.Thumbprint;
+
+                        Console.WriteLine();
+                    }
+
+                    string strCmdText = "netsh http delete sslcert ipport=0.0.0.0:" +Constants.ServicePort;
+                    Console.WriteLine(strCmdText);
+                    strCmdText = "netsh http add sslcert ipport=0.0.0.0:"+ Constants.ServicePort + " certhash=" + FinalThumbprint.Trim() + " appid={"+ Constants.ServiceAppGUID +"}";
+                    Console.WriteLine(strCmdText);
+
+                    Process cmd = new Process();
+                    cmd.StartInfo.FileName = "cmd.exe";
+                    cmd.StartInfo.RedirectStandardInput = true;
+                    cmd.StartInfo.RedirectStandardOutput = true;
+                    cmd.StartInfo.CreateNoWindow = true;
+                    cmd.StartInfo.UseShellExecute = false;
+                    cmd.Start();
+
+                    cmd.StandardInput.WriteLine("netsh http delete sslcert ipport=0.0.0.0:" + Constants.ServicePort);
+                    cmd.StandardInput.WriteLine("netsh http add sslcert ipport=0.0.0.0:"+ Constants.ServicePort + " certhash=" + FinalThumbprint.Trim() + " appid={" + Constants.ServiceAppGUID + "}";
+                    cmd.StandardInput.Flush();
+                    cmd.StandardInput.Close();
+                    cmd.WaitForExit();
+                    Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+
+                    store.Close();
+                    Console.ReadLine();
                 }
-
-                string strCmdText = "netsh http delete sslcert ipport=0.0.0.0:9000";
-                Console.WriteLine(strCmdText);
-                strCmdText = "netsh http add sslcert ipport=0.0.0.0:9000 certhash=" + FinalThumbprint.Trim() + " appid={3a487f59-fcb7-4213-b7ff-871456ae7b70}";
-                Console.WriteLine(strCmdText);
-
-                Process cmd = new Process();
-                cmd.StartInfo.FileName = "cmd.exe";
-                cmd.StartInfo.RedirectStandardInput = true;
-                cmd.StartInfo.RedirectStandardOutput = true;
-                cmd.StartInfo.CreateNoWindow = true;
-                cmd.StartInfo.UseShellExecute = false;
-                cmd.Start();
-
-                cmd.StandardInput.WriteLine("netsh http delete sslcert ipport=0.0.0.0:9000");
-                cmd.StandardInput.WriteLine("netsh http add sslcert ipport=0.0.0.0:9000 certhash=" + FinalThumbprint.Trim() + " appid={3a487f59-fcb7-4213-b7ff-871456ae7b70}");
-                cmd.StandardInput.Flush();
-                cmd.StandardInput.Close();
-                cmd.WaitForExit();
-                Console.WriteLine(cmd.StandardOutput.ReadToEnd());
-
-                store.Close();
-                Console.ReadLine();
             }
-            catch (CryptographicException)
+            catch (CryptographicException e)
             {
-                Console.WriteLine("Information could not be written out for this certificate.");
+                Console.WriteLine("Information could not be written out for this certificate. The error code is:  " + e);
             }
         }
     }
