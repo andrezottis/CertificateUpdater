1fd19d98e68c26e7e8438611cfc0a4623a797012
André Zottis <zottis.andre@outlook.com>
2019-05-09 16:55:41 -0300

    Add Assembly information. Add Icon.

--- CertificateUpdater/CertificateUpdater/Properties/AssemblyInfo.cs
+++ CertificateUpdater/CertificateUpdater/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CertificateUpdater")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Updates certificates for Selfhosting Web Application.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Made by https://github.com/andrezottis")]
 [assembly: AssemblyProduct("CertificateUpdater")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-AT")]
+
